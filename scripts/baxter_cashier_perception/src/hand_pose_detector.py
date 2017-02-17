2e2efe03db47f4b2ae4d57fc9bbb725db851241e
Rafael Papallas <sc13rp@leeds.ac.uk>
2017-02-17 18:13:07 +0000

    Minor changes

--- scripts/baxter_cashier_perception/src/hand_pose_detector.py
+++ scripts/baxter_cashier_perception/src/hand_pose_detector.py
@@ -8,18 +8,11 @@ import rospy
 import math
 import tf
 import geometry_msgs.msg
-import turtlesim.srv
 
 if __name__ == '__main__':
     rospy.init_node('hand_pose_detector')
 
     listener = tf.TransformListener()
-
-    # rospy.wait_for_service('spawn')
-    # spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
-    # spawner(4, 2, 0, 'turtle2')
-
-    # turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist,queue_size=1)
 
     rate = rospy.Rate(0.1)
     while not rospy.is_shutdown():
@@ -28,12 +21,5 @@ if __name__ == '__main__':
             print str(trans) + ", " + str(rot)
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-
-        # angular = 4 * math.atan2(trans[1], trans[0])
-        # linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-        # cmd = geometry_msgs.msg.Twist()
-        # cmd.linear.x = linear
-        # cmd.angular.z = angular
-        # turtle_vel.publish(cmd)
 
         rate.sleep()
