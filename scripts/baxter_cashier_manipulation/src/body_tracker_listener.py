2f5ea0201db14316a3e46d594473964fb2a5f47e
Rafael Papallas <sc13rp@leeds.ac.uk>
2017-02-27 15:12:17 +0000

    Added body tracker in the same directory since python couldn't located it

---
+++ scripts/baxter_cashier_manipulation/src/body_tracker_listener.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+"""
+This script acts as a listener to the Skeleton Tracker provided by the
+cob_people_perception library. It listens to a specific frame of the
+user's hand and returns the pose to the caller.
+
+The cob_people_perception library publishes the tf frames in the following
+manner:
+- Parent: /camera_depth_optical_frame
+- Childs: /cob_body_tracker/user_n/{x}
+  - Where n is the number of the user (1, 2, 3 etc)
+  - Where x is the body part with possible options:
+    - Shoulders: left_shoulder, right_shoulder
+    - Elbows: left_elbow, right_elbow
+    - Hands: left_hand, right_hand
+    - Hips: left_hip, right_hip
+    - Knees: left_knee, right_knee
+    - Feet: left_foot, right_foot
+    - Other: head, neck, torso
+"""
+
+import roslib
+import os
+import rospy
+import tf
+import time
+import subprocess
+
+
+class InvalidBodyPartException(Exception):
+    """
+    Raises exception if the body part string provided by the user is
+    not a valid one (i.e not provided by the skeleton tracker)
+    """
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return """ The provided body part {} is not a valid body part
+               for the skeleton tracker.""".format(self.value)
+
+
+class BodyTrackerListener:
+    def __init__(self):
+        # General default configuration for the listener
+        # rospy.init_node("body_tracker_listener")
+        self._listener = tf.TransformListener()
+        self._RATE = rospy.Rate(0.3)
+
+    def _is_body_part_valid(self, body_part):
+        """
+        Given a body_part will return true if the part is valid or
+        false if is invalid.
+        """
+        possible_body_parts = ['head', 'neck', 'torso', 'left_shoulder',
+                               'right_shoulder', 'left_elbow', 'right_elbow',
+                               'left_hand', 'right_hand', 'left_hip',
+                               'right_hip', 'left_knee', 'right_knee',
+                               'left_foot', 'right_foot']
+
+        return True if body_part in possible_body_parts else False
+
+    def start_listening_for(self, user_number, body_part):
+        """
+        Bridge method that starts the Skeleton Tracker, starts the process of
+        listening to the pose and kill the skeleton tracker.
+
+        Returns back the transformation and rotation of the pose requested.
+        """
+        # Throw an exception if body part not valid
+        if not self._is_body_part_valid(body_part):
+            try:
+                raise InvalidBodyPartException(body_part)
+            except InvalidBodyPartException as e:
+                print e
+
+        print "Start listenning..."
+        tran, rot = self._listen(user_number=user_number,  body_part=body_part)
+        print "Finished listenning."
+
+        return tran, rot
+
+    def _listen(self, user_number, body_part):
+        # Source is the node parent and target the child we are looking for.
+        source = '/camera_depth_optical_frame'
+        target = "cob_body_tracker/user_{}/{}".format(user_number, body_part)
+
+        timeout_start = time.time()
+        timeout = 5   # [seconds]
+
+        transformation = [0, 0, 0]
+        rotation = [0, 0, 0, 0]
+
+        while time.time() < timeout_start + timeout:
+            try:
+                # Try to listen for the transformation and rotation of the node
+                (transformation, rotation) =self. _listener.lookupTransform(source,
+                                                                       target,
+                                                                       rospy.Time(0))
+            except (tf.LookupException, tf.ConnectivityException,
+                    tf.ExtrapolationException) as e:
+                print e
+
+            self._RATE.sleep()
+
+        return transformation, rotation
+
+
+if __name__ == '__main__':
+    tracker_listener = BodyTrackerListener()
+    tran, rot = tracker_listener.start_listening_for(user_number=1,
+                                                     body_part="left_hand")
+
+    print "Transformation: {}".format(tran)
+    print "Rotation: {}".format(rot)
