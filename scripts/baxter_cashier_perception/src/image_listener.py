fe86eaa0bf7fe59f835eff14e021f77d9f28c4b4
Rafael Papallas <sc13rp@leeds.ac.uk>
2017-03-02 20:26:49 +0000

    Converted class to a ROS Service

--- scripts/baxter_cashier_perception/src/image_listener.py
+++ scripts/baxter_cashier_perception/src/image_listener.py
@@ -10,14 +10,25 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
+from baxter_cashier_manipulation.srv import RecogniseBanknote
 
 
-class ImageListener:
-    def __init__(self, image_topic):
+class BanknoteRecogniser:
+    def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber(image_topic, Image, self.callback)
-        self.counter = 0
-        self.detected = None
+        self.detected_amount = None
+
+    def detect(self, request):
+        self.image_sub = rospy.Subscriber(request.camera_topic,
+                                          Image,
+                                          self.callback)
+
+        while True:
+            if self.detected_amount is not None:
+                self.image_sub.unregister()
+                response = RecogniseBanknoteResponse(self.detected_amount)
+                self.detected_amount = None
+                return response
 
     def callback(self, data):
         try:
@@ -25,15 +36,6 @@ class ImageListener:
         except CvBridgeError as e:
             print e
 
-        if self.counter < 5:
-            self.counter += 1
-            return
-
-        if self.counter == 5:
-            print "Now tracking..."
-            self.counter += 1
-
-        # The 1 means we want the image in BGR, and not in grayscale.
         img = cv_image
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -43,31 +45,27 @@ class ImageListener:
         lower_range_orange = np.array([1, 100, 100])
         upper_range_orange = np.array([101, 255, 255])
 
-        hsv2 = hsv.copy()
-
         mask_orange = cv2.inRange(hsv, lower_range_orange, upper_range_orange)
-        mask_red = cv2.inRange(hsv2, lower_range_red, upper_range_red)
+        mask_red = cv2.inRange(hsv, lower_range_red, upper_range_red)
 
-        if np.count_nonzero(mask_red) > 40000:
+        if np.count_nonzero(mask_red) > 40000 or np.count_nonzero(mask_orange) > 40000:
             if np.count_nonzero(mask_red) > np.count_nonzero(mask_orange):
-                if self.detected == "five" or self.detected is None:
-                    print "Detected a ONE"
-                    self.detected = "one"
+                print "Detected a ONE"
+                self.detected_amount = 1
 
-        if np.count_nonzero(mask_orange) > 40000:
             if np.count_nonzero(mask_orange) > np.count_nonzero(mask_red):
-                if self.detected == "one" or self.detected is None:
-                    print "Detected a FIVE"
-                    self.detected = "five"
+                print "Detected a FIVE"
+                self.detected_amount = 5
 
 
 if __name__ == '__main__':
-    ic = ImageListener("/camera/rgb/image_rect_color")
-    rospy.init_node('image_listener', anonymous=True)
+    image_topic = "/camera/rgb/image_rect_color"
+    banknote_recogniser = BanknoteRecogniser()
 
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print "Shutting down"
+    rospy.init_node('bank_note_recogniser', anonymous=True)
 
-    cv2.destroyAllWindows()
+    s = rospy.Service('recognise_banknote',
+                      RecogniseBanknote,
+                      banknote_recogniser.detect)
+
+    rospy.spin()
