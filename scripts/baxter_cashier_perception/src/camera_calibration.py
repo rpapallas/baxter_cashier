9668a5a29c15f1e812507e3af9706a841af86c6b
Rafael Papallas <sc13rp@leeds.ac.uk>
2017-03-04 14:54:58 +0000

    Fixed som runt-time errors

--- scripts/baxter_cashier_perception/src/camera_calibration.py
+++ scripts/baxter_cashier_perception/src/camera_calibration.py
@@ -2,8 +2,7 @@
 
 """
 This class allows two cameras to align with each other by publishing a tf.
-
-Creator: Muhannad Al-Omari, University of Leeds
+Creator: Muhannad, University of Leeds
 """
 
 import rospy
@@ -26,6 +25,7 @@ class Calibrator:
         self.rpy = [0, 0, 0]
         self.rpy_calib = [2343, 10, 2701]  # the values you should use
         self.rate = rospy.Rate(100.0)
+        self.q1 = [0.0, 0.0, 0.0, 1.0]
 
         # The class' brodcasters
         self.brodcaster_1 = tf.TransformBroadcaster()
@@ -35,24 +35,22 @@ class Calibrator:
         self.base_topic = base_topic
         self.target_topic = target_topic
 
-        # Creates the CV Interface with trackbars to tune values
-        self.cv2 = _create_trackbars()
+        self.cv2 = cv2
 
-    def _create_trackbars(self):
-        cv2.namedWindow('image')
+
+        self.cv2.namedWindow('image')
 
         # Create trackbars for colour change
-        cv2.createTrackbar('x', 'image', 0, 3000, self._on_trackbar_change)
-        cv2.createTrackbar('y', 'image', 0, 3000, self._on_trackbar_change)
-        cv2.createTrackbar('z', 'image', 0, 3000, self._on_trackbar_change)
+        self.cv2.createTrackbar('x', 'image', 0, 3000, self._on_trackbar_change)
+        self.cv2.createTrackbar('y', 'image', 0, 3000, self._on_trackbar_change)
+        self.cv2.createTrackbar('z', 'image', 0, 3000, self._on_trackbar_change)
 
-        cv2.createTrackbar('Roll',  'image', 0, 3600, self._on_trackbar_change)
-        cv2.createTrackbar('Pitch', 'image', 0, 3600, self._on_trackbar_change)
-        cv2.createTrackbar('Yaw',   'image', 0, 3600, self._on_trackbar_change)
+        self.cv2.createTrackbar('Roll',  'image', 0, 3600 ,self. _on_trackbar_change)
+        self.cv2.createTrackbar('Pitch', 'image', 0, 3600, self._on_trackbar_change)
+        self.cv2.createTrackbar('Yaw',   'image', 0, 3600,self. _on_trackbar_change)
 
-        return cv2
-
-    def _on_trackbar_change(self):
+    # Creates the CV Interface with trackbars to tune values
+    def _on_trackbar_change(self, self2):
         # Tune xyz and rpy with the current positions of the trackbars
         self.xyz = self._extract_xyz_from_trackbars()
         self.rpy = self._extract_rpy_from_trackbars()
@@ -60,7 +58,7 @@ class Calibrator:
         # Using List Comprehension we get new values for r, p and y
         # based on static formula computed by `_get_new_value_for` static
         # method
-        roll, pitch, yaw  = [_get_new_value_for(x) for x in self.rpy]
+        roll, pitch, yaw = map(self._get_new_value_for, self.rpy)
 
         # Using Euler method calculates the quaternion from roll, pitch and yaw
         self.quaternion = tf.transformations.quaternion_from_euler(roll,
@@ -92,6 +90,20 @@ class Calibrator:
     def calibrate(self):
         """Method performs the basic operation."""
         while not rospy.is_shutdown():
+            img = np.zeros((300,512,3), np.uint8)
+            self.cv2.imshow('image', img)
+            k = self.cv2.waitKey(1) & 0xFF
+            print 'xyz:', self.xyz
+            print 'rpy:', self. rpy
+            print 'q:', self. quaternion
+            print '------------------'
+
+            self.brodcaster_2.sendTransform(tuple(self.xyz),
+                                                                  tuple(self.q1),
+                                                                  rospy.Time.now(),
+                                                                  self.base_topic,
+                                                                  self.target_topic)
+
             self.brodcaster_1.sendTransform(tuple(self.xyz_calib1),
                                             tuple(self.q_calib1),
                                             rospy.Time.now(),
@@ -101,6 +113,6 @@ class Calibrator:
 
 
 if __name__ == '__main__':
-    calibrator = Calibrator(base_topic="kinect2_1_ir_optical_frame",
+    calibrator = Calibrator(base_topic="camera_depth_optical_frame",
                             target_topic="torso")
     calibrator.calibrate()
