7ba6b4fd829b59cf6aaa54477d451a452e269156
Rafael Papallas <sc13rp@leeds.ac.uk>
2017-03-04 22:02:46 +0000

    Minor changes
    Signed-off-by: Rafael Papallas <sc13rp@leeds.ac.uk>

--- scripts/baxter_cashier_perception/src/camera_calibration.py
+++ scripts/baxter_cashier_perception/src/camera_calibration.py
@@ -17,40 +17,39 @@ class Calibrator:
     def __init__(self, base_topic, target_topic):
         """Default constructor configuring the class properties."""
         rospy.init_node('camera_calibrator_tf_broadcaster')
+        self.rate = rospy.Rate(100.0)
 
+        # Some initialisation
         self.quaternion = None
         self.xyz = [0.0, 0.0, 0.0]
-        self.q_calib1 = [-0.62690571,  0.63143915, -0.31687864,  0.32842314]
-        self.xyz_calib1 = [0.272, -0.03600000000000003, 0.23399999999999999]
         self.rpy = [0, 0, 0]
-        self.rpy_calib = [2343, 10, 2701]  # the values you should use
-        self.rate = rospy.Rate(100.0)
-        self.q1 = [0.0, 0.0, 0.0, 1.0]
 
         # The class' brodcasters
         self.brodcaster_1 = tf.TransformBroadcaster()
-        self.brodcaster_2 = tf.TransformBroadcaster()
 
         # Topics that will be used to broadcast tf
         self.base_topic = base_topic
         self.target_topic = target_topic
 
-        self.cv2 = cv2
+        # Initialise the trackbars (sliders) for the CV window
+        self._create_trackbars_for_window()
 
-
+    def _create_trackbars_for_window(self):
         self.cv2.namedWindow('image')
 
-        # Create trackbars for colour change
-        self.cv2.createTrackbar('x', 'image', 0, 3000, self._on_trackbar_change)
-        self.cv2.createTrackbar('y', 'image', 0, 3000, self._on_trackbar_change)
-        self.cv2.createTrackbar('z', 'image', 0, 3000, self._on_trackbar_change)
+        # Create trackbars
+        self.cv2.createTrackbar('x', 'image', 0, 3000, self._callback)
+        self.cv2.createTrackbar('y', 'image', 0, 3000, self._callback)
+        self.cv2.createTrackbar('z', 'image', 0, 3000, self._callback)
 
-        self.cv2.createTrackbar('Roll',  'image', 0, 3600 ,self. _on_trackbar_change)
-        self.cv2.createTrackbar('Pitch', 'image', 0, 3600, self._on_trackbar_change)
-        self.cv2.createTrackbar('Yaw',   'image', 0, 3600,self. _on_trackbar_change)
+        self.cv2.createTrackbar('Roll',  'image', 0, 3600, self._callback)
+        self.cv2.createTrackbar('Pitch', 'image', 0, 3600, self._callback)
+        self.cv2.createTrackbar('Yaw',   'image', 0, 3600, self._callback)
 
-    # Creates the CV Interface with trackbars to tune values
-    def _on_trackbar_change(self, self2):
+    def _callback(self, _):
+        def apply_formula(value):
+            return value * np.pi / 1800
+
         # Tune xyz and rpy with the current positions of the trackbars
         self.xyz = self._extract_xyz_from_trackbars()
         self.rpy = self._extract_rpy_from_trackbars()
@@ -58,7 +57,7 @@ class Calibrator:
         # Using List Comprehension we get new values for r, p and y
         # based on static formula computed by `_get_new_value_for` static
         # method
-        roll, pitch, yaw = map(self._get_new_value_for, self.rpy)
+        roll, pitch, yaw = map(apply_formula, self.rpy)
 
         # Using Euler method calculates the quaternion from roll, pitch and yaw
         self.quaternion = tf.transformations.quaternion_from_euler(roll,
@@ -83,32 +82,27 @@ class Calibrator:
 
         return [r, p, y]
 
-    @staticmethod
-    def _get_new_value_for(value):
-        return value * np.pi / 1800
+    def _print_current_configuration(self, results):
+        print "xyz: {} | rpy: {} | q: {}".format(self.xyz,
+                                                 self.rpy,
+                                                 self.quaternion)
 
     def calibrate(self):
         """Method performs the basic operation."""
+
         while not rospy.is_shutdown():
-            img = np.zeros((300,512,3), np.uint8)
+            img = np.zeros((300, 512, 3), np.uint8)
             self.cv2.imshow('image', img)
-            k = self.cv2.waitKey(1) & 0xFF
-            print 'xyz:', self.xyz
-            print 'rpy:', self. rpy
-            print 'q:', self. quaternion
-            print '------------------'
+            _ = self.cv2.waitKey(1) & 0xFF
 
-            self.brodcaster_2.sendTransform(tuple(self.xyz),
-                                                                  tuple(self.q1),
-                                                                  rospy.Time.now(),
-                                                                  self.base_topic,
-                                                                  self.target_topic)
+            self._print_current_configuration()
 
-            self.brodcaster_1.sendTransform(tuple(self.xyz_calib1),
-                                            tuple(self.q_calib1),
+            self.brodcaster_1.sendTransform(tuple(self.xyz),
+                                            tuple(self.quaternion),
                                             rospy.Time.now(),
                                             self.base_topic,
                                             self.target_topic)
+
             self.rate.sleep()
 
 
